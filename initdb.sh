a74ba553e1c1b56882403f40ad31755e2a88b305
Paul Stevenson <pstevenson6@gmail.com>
2019-10-05 21:52:46 +0800

    refactored database, docker-comopse has most containers commented out for dev

--- initdb.sh
+++ initdb.sh
@@ -3,83 +3,61 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<- EOSQL
 
-      CREATE TABLE expenses(
-         id BIGINT PRIMARY KEY,
-         date DATE,
-         who BIGINT,
-         category INT,
-         amount REAL,
-         store BIGINT,
-         gid BIGINT,
-         deleted BOOLEAN DEFAULT FALSE,
-         notes VARCHAR (140),
-         mod_timestamp TIMESTAMP,
-         mod_user BIGINT,
-         root_id BIGINT
-      );
-      CREATE SEQUENCE expenses_id_seq MINVALUE 1;
-      ALTER TABLE expenses ALTER id SET DEFAULT nextval('expenses_id_seq');
-      ALTER SEQUENCE expenses_id_seq OWNED BY expenses.id;
-
-
-
-      CREATE TABLE exp_group (
-          id BIGINT PRIMARY KEY,
+      CREATE TABLE groups (
+          id SERIAL PRIMARY KEY,
           name VARCHAR (50) DEFAULT 'My first group'::text,
           deleted BOOLEAN DEFAULT FALSE,
           tier INT DEFAULT 1
       );
-      CREATE SEQUENCE exp_group_id_seq MINVALUE 1;
-      ALTER TABLE exp_group ALTER id SET DEFAULT nextval('exp_group_id_seq');
-      ALTER SEQUENCE exp_group_id_seq OWNED BY exp_group.id;
-
-
 
       CREATE TABLE categories (
-          id BIGINT PRIMARY KEY,
+          id SERIAL PRIMARY KEY,
           name VARCHAR (50),
           parent VARCHAR (50)
       );
-      CREATE SEQUENCE categories_id_seq MINVALUE 1;
-      ALTER TABLE categories ALTER id SET DEFAULT nextval('categories_id_seq');
-      ALTER SEQUENCE categories_id_seq OWNED BY categories.id;
 
-
-
-      CREATE TABLE people (
-          id BIGINT PRIMARY KEY,
+      CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
           first VARCHAR (50) DEFAULT ''::text,
           last VARCHAR (50) DEFAULT ''::text,
           email VARCHAR (50),
           deleted BOOLEAN DEFAULT FALSE
       );
-      CREATE SEQUENCE people_id_seq MINVALUE 1;
-      ALTER TABLE people ALTER id SET DEFAULT nextval('people_id_seq');
-      ALTER SEQUENCE people_id_seq OWNED BY people.id;
-
-
 
       CREATE TABLE stores (
-          id BIGINT PRIMARY KEY,
+          id SERIAL PRIMARY KEY,
           name VARCHAR (50),
           entity VARCHAR (50),
           city VARCHAR (50),
           deleted BOOLEAN DEFAULT FALSE
       );
-      CREATE SEQUENCE stores_id_seq MINVALUE 1;
-      ALTER TABLE stores ALTER id SET DEFAULT nextval('stores_id_seq');
-      ALTER SEQUENCE stores_id_seq OWNED BY stores.id;
 
-
-
-      CREATE TABLE user_group (
-          id BIGINT PRIMARY KEY,
-          uid BIGINT,
-          gid BIGINT,
-          admin BOOLEAN DEFAULT FALSE
+      CREATE TABLE expenses(
+         id SERIAL PRIMARY KEY,
+         date DATE,
+         user_id BIGINT,
+         category BIGINT,
+         amount REAL,
+         store BIGINT,
+         group_id BIGINT,
+         deleted BOOLEAN DEFAULT FALSE,
+         notes VARCHAR (140),
+         mod_timestamp timestamp NOT NULL DEFAULT NOW(),
+         mod_user BIGINT,
+         root_id BIGINT,
+         FOREIGN KEY (group_id) REFERENCES groups(id),
+         FOREIGN KEY (category) REFERENCES categories(id),
+         FOREIGN KEY (store) REFERENCES stores(id),
+         FOREIGN KEY (root_id) REFERENCES expenses(id)
       );
-      CREATE SEQUENCE user_group_id_seq MINVALUE 1;
-      ALTER TABLE user_group ALTER id SET DEFAULT nextval('user_group_id_seq');
-      ALTER SEQUENCE user_group_id_seq OWNED BY user_group.id;
+
+      CREATE TABLE users_groups (
+          user_id BIGINT NOT NULL,
+          group_id BIGINT NOT NULL,
+          admin BOOLEAN DEFAULT FALSE,
+          PRIMARY KEY (user_id, group_id),
+          FOREIGN KEY (user_id) REFERENCES users(id),
+          FOREIGN KEY (group_id) REFERENCES groups(id)
+      );
 
 EOSQL
